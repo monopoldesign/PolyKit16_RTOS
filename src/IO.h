9391579370376cb592db6a19c691f1ab28b92f2d
mvolkel <mario.volkel@outlook.com>
2025-05-13 18:50:52 +0100

    First Buttons added

--- src/IO.h
+++ src/IO.h
@@ -1,33 +1,56 @@
+#ifndef _IO_H_
+#define _IO_H_
+
 /*----------------------------------------------------------------------------
 - PolyKit16 - FreeRTOS-Version
 - (C)2025 M.Volkel (mario.volkel@outlook.com)
 -
-- Main-Program
+- IO-Functions
 ----------------------------------------------------------------------------*/
 
 /*----------------------------------------------------------------------------
 - Header-Files
 ----------------------------------------------------------------------------*/
-#include "main.h"
 
 /*----------------------------------------------------------------------------
-- Global Variables
+- Definitions
 ----------------------------------------------------------------------------*/
+#define TD_GETINPUTS	1
+
+#define TP_GETINPUTS	(tskIDLE_PRIORITY + 2)
+
+#define OCTO_TOTAL 2
+
+#define PIN_DATA		50	// pin 9 on 74HC165 (DATA)
+#define PIN_LOAD		49  // pin 1 on 74HC165 (LOAD)
+#define PIN_CLK			51  // pin 2 on 74HC165 (CLK))
 
 /*----------------------------------------------------------------------------
-- Functions
+- Class
 ----------------------------------------------------------------------------*/
-
-void setup()
+class IO
 {
-	while (!Serial)
-		yield();
+	public:
+			// Class-Functions
+			IO();
+			~IO();
 
-	IO::init();
+			// Task-Functions
+			static void createTasks();
 
-	IO::createTasks();
-}
+			// Init-Functions
+			static void init();
 
-void loop()
-{
-}
+private:
+			// Task-Functions
+			static void task_GetInputs(void *arg);
+
+			// Init-Functions
+			static void initIOPins();
+
+			// Hardware-Pin-Functions
+			static void getInputSR();
+			static uint8_t readShiftReg(uint32_t dataPin, uint32_t clkPin);
+};
+
+#endif
